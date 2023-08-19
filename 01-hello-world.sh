a1b3c066e4e527e73b42634f4d9dfcf3a438e62d
Shaik Khizar <creativekhizar1279@gmail.com>
2023-08-19 11:22:29 +0530

    changes made

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -5,7 +5,7 @@ echo "Hello World, I am Learning DevOps with AWS"
 person1="Sachin"
 person2="Rohith"
 
-echo "$person1: Hi Suresh, Good Morning"
+echo "$person1: Hi $person2, Good Morning"
 echo "$person2: Good Morning $person2"
 echo "$person1: How are you $person1"
 echo "$person2: I am Good, How are you $person1"
