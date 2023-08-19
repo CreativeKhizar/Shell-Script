f4bcfc24e1323038479268b84728e2c8d9aa2025
Shaik Khizar <creativekhizar1279@gmail.com>
2023-08-19 11:45:14 +0530

    changes made

--- 05-variables.sh
+++ 05-variables.sh
@@ -6,6 +6,8 @@ read USERNAME # this is the variable where the value of the user entered
 
 echo "User Name Entered is $USERNAME"
 
+# this is only for validation, we should not print like this
+
 echo "Please Enter your Password"
 
 read -s PASSWORD
---
+++ 06-array.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# This is called as Array instead of single value it can hold multiple values
+PERSONS=("Khizar","Macha","Prakash","Madhav")
+
+echo "First Person: ${PERSONS[0]}"
