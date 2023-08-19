6f8a93abf546287b368183a473a65b1ec23f47bb
Shaik Khizar <creativekhizar1279@gmail.com>
2023-08-19 11:37:12 +0530

    changes made

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
-echo "Please Enter your Username : "
+echo "Please Enter your Username "
 
 read USERNAME # this is the variable where the value of the user entered
 
 echo "User Name Entered is $USERNAME"
+
+echo "Please Enter your Password"
+
+read PASSWORD
+
+echo "Password entered is: $PASSWORD"
