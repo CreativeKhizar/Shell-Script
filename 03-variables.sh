e36c90bcdfabd778b125d1e996b529b578435fc8
Shaik Khizar <creativekhizar1279@gmail.com>
2023-08-19 11:27:50 +0530

    changes made

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+person1=$1
+person2=$2
+
+echo "$person1: Hi $person2, Good Morning"
+echo "$person2: Good Morning $person2"
+echo "$person1: How are you $person1"
+echo "$person2: I am Good, How are you $person1"
