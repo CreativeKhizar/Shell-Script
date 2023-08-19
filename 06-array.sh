d674fac4d8b26865f1d9e8a10b0b7b3ceb68132f
Shaik Khizar <creativekhizar1279@gmail.com>
2023-08-19 11:46:47 +0530

    changes made

--- 06-array.sh
+++ 06-array.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 # This is called as Array instead of single value it can hold multiple values
-PERSONS=("Khizar","Macha","Prakash","Madhav")
+PERSONS=("Khizar" "Macha" "Prakash" "Madhav")
 
 echo "First Person: ${PERSONS[0]}"
+
+echo "All Persons: ${PERSONS[@]}"
